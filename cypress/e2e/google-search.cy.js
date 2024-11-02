96c521411f33cd2db9e8aa632972da7e1e66800c
Micah Wood <micah@wpscholar.com>
2024-11-02 12:44:53 -0400

    Get the search box

--- cypress/e2e/google-search.cy.js
+++ cypress/e2e/google-search.cy.js
@@ -1,5 +1,6 @@
 describe('Perform Google Search', () => {
     it('Validate that wpscholar.com shows on the first page', () => {
         cy.visit('https://www.google.com')
+        cy.get('[name="q"]')
     })
 })
