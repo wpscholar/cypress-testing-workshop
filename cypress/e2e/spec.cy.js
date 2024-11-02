de1bece4a4f0c2e3109cbe93afb26410cbcaf578
Micah Wood <micah@wpscholar.com>
2024-11-02 12:16:58 -0400

    Change test description

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('My First Test', () => {
+  it('Gets, types, and asserts', () => {
     cy.visit('https://example.cypress.io')
   })
 })
