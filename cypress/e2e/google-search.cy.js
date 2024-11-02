32adadbe7489c140f0df4e2a5ba8256753b50816
Micah Wood <micah@wpscholar.com>
2024-11-02 12:56:50 -0400

    Validate that `wpscholar.com` shows in results

--- cypress/e2e/google-search.cy.js
+++ cypress/e2e/google-search.cy.js
@@ -2,5 +2,6 @@ describe('Perform Google Search', () => {
     it('Validate that wpscholar.com shows on the first page', () => {
         cy.visit('https://www.google.com')
         cy.get('[name="q"]').type('wpscholar{enter}')
+        cy.contains('wpscholar.com')
     })
 })
