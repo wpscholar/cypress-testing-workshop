0d386b48c298c84c0075cc98192ddf32ec478c6c
Micah Wood <micah@wpscholar.com>
2024-11-02 12:19:27 -0400

    Get the email field on the page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,5 +3,6 @@ describe('My First Test', () => {
     cy.visit('https://example.cypress.io')
     cy.contains('type').click()
     cy.url().should('include', '/commands/actions')
+    cy.get('.action-email')
   })
 })
