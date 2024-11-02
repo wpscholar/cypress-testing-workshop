2606f37fb67300f2020468ca06e43cec9b617214
Micah Wood <micah@wpscholar.com>
2024-11-02 12:20:15 -0400

    Validate the email field value

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,5 +4,7 @@ describe('My First Test', () => {
     cy.contains('type').click()
     cy.url().should('include', '/commands/actions')
     cy.get('.action-email').type('fake@email.com')
+    cy.get('.action-email')
+      .should('have.value', 'fake@email.com')
   })
 })
