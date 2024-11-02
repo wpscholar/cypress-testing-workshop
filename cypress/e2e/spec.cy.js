236108d51a9112f79af07c1768dbc4aa6497511b
Micah Wood <micah@wpscholar.com>
2024-11-02 12:19:06 -0400

    Validate that it takes us to the correct URL

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,5 +2,6 @@ describe('My First Test', () => {
   it('Gets, types, and asserts', () => {
     cy.visit('https://example.cypress.io')
     cy.contains('type').click()
+    cy.url().should('include', '/commands/actions')
   })
 })
