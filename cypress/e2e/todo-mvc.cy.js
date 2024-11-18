662c0e73981c44c125f23deb7ba4904d491f1e85
Micah Wood <micah@wpscholar.com>
2024-11-18 00:11:51 -0500

    No need for .first() here

--- cypress/e2e/todo-mvc.cy.js
+++ cypress/e2e/todo-mvc.cy.js
@@ -20,7 +20,7 @@ describe('TODO MVC', () => {
 
     it('Edit a task', () => {
         cy.get('[data-testid="text-input"]').type('Learn Cypress').type('{enter}')
-        cy.get('[data-testid="todo-list"]').first().contains('Learn Cypress').dblclick()
+        cy.get('[data-testid="todo-list"]').contains('Learn Cypress').dblclick()
         cy.get('[data-testid="todo-list"]').first().find('input').clear().type('Teach Cypress').type('{enter}')
         cy.get('[data-testid="todo-list"]').first().should('contain', 'Teach Cypress')
     })
