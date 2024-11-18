28001480b9690fc8a3eb718f298feb8a429cc76d
Micah Wood <micah@wpscholar.com>
2024-11-17 23:40:12 -0500

    Add test for marking all tasks as active

--- cypress/e2e/todo-mvc.cy.js
+++ cypress/e2e/todo-mvc.cy.js
@@ -63,7 +63,13 @@ describe('TODO MVC', () => {
     })
 
     it('Mark all tasks active', () => {
-
+        cy.get('.new-todo').type('Learn Cypress').type('{enter}')
+        cy.get('.new-todo').type('Teach Cypress').type('{enter}')
+        cy.get('.todo-list li.completed').should('have.length', 0)
+        cy.get('.toggle-all').click()
+        cy.get('.todo-list li.completed').should('have.length', 2)
+        cy.get('.toggle-all').click()
+        cy.get('.todo-list li.completed').should('have.length', 0)
     })
 
 })
