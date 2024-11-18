ac468857ffd33c63ca9e7b3f5598f1e116104c14
Micah Wood <micah@wpscholar.com>
2024-11-18 00:21:55 -0500

    Update “Clear completed tasks” test to use test IDs

--- cypress/e2e/todo-mvc.cy.js
+++ cypress/e2e/todo-mvc.cy.js
@@ -46,12 +46,12 @@ describe('TODO MVC', () => {
     })
 
     it('Clear completed tasks', () => {
-        cy.get('.new-todo').type('Learn Cypress').type('{enter}')
-        cy.get('.new-todo').type('Teach Cypress').type('{enter}')
-        cy.get('.todo-list li').should('have.length', 2)
-        cy.get('.todo-list li').first().find('.toggle').click()
+        cy.get('[data-testid="text-input"]').type('Learn Cypress').type('{enter}')
+        cy.get('[data-testid="text-input"]').type('Teach Cypress').type('{enter}')
+        cy.get('[data-testid="todo-item"]').should('have.length', 2)
+        cy.get('[data-testid="todo-item-toggle"]').first().click()
         cy.get('.clear-completed').click()
-        cy.get('.todo-list li').should('have.length', 1)
+        cy.get('[data-testid="todo-list"]').should('have.length', 1)
     })
 
     it('Mark all tasks complete', () => {
