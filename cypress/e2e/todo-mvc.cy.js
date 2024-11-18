7a895f40f7883b8ad1af1fee1b4c8ceb2ca46455
Micah Wood <micah@wpscholar.com>
2024-11-17 23:36:10 -0500

    Create placeholders for what we need to test

--- cypress/e2e/todo-mvc.cy.js
+++ cypress/e2e/todo-mvc.cy.js
@@ -2,6 +2,36 @@
 
 describe('TODO MVC', () => {
 
+    it('Add a new task', () => {
 
+    })
+
+    it('Mark a task complete', () => {
+
+    })
+
+    it('Edit a task', () => {
+
+    })
+
+    it('Delete a task', () => {
+
+    })
+
+    it('Filter tasks', () => {
+
+    })
+
+    it('Clear completed tasks', () => {
+
+    })
+
+    it('Mark all tasks complete', () => {
+
+    })
+
+    it('Mark all tasks active', () => {
+
+    })
 
 })
